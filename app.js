98f487faefe909116b2cf400a24974738b865d89
Syvit <collin.trumpler@gmail.com>
2023-04-29 10:16:48 -0400

    refined layout and added null input response

--- app.js
+++ app.js
@@ -15,55 +15,73 @@ const library = (function(){
     openForm.addEventListener('click', formDisplay);
 
     function addBook(){
-        
         const info = {
             title: titleInput.value,
             author: authorInput.value, 
             check: checkBox.checked
             }
 
-        library.push(info)
 
-        const newBook = document.createElement('li');
-        const newBookTitle = document.createElement('h1');
-        const newBookAuthor = document.createElement('h2');
-        const newBookCheck = document.createElement('h2');
-
-        newBook.appendChild(newBookTitle);
-        newBook.appendChild(newBookAuthor);
-        newBook.appendChild(newBookCheck);
-
-        function checked(){
-            if (info.check == true){
-                return 'Complete'
+            if(info.title === ""){
+                alert('Title cannot be blank')
+                return;
             }
-            else return 'Incomplete'
-        }
-        
-        newBookTitle.innerHTML += `Title: ${info.title}`
-        newBookAuthor.innerHTML += `Author: ${info.author}`
-        newBookCheck.innerHTML += `Status: ${checked()}`
 
-        listOutput.appendChild(newBook);
-        newBook.className = ('book-entry');
+            else if(info.author === ""){
+                alert('Author cannot be blank')
+                return;
+            }
 
-        for(const value of Object.keys(library)){
-            newBook.id = (value);
-        }
-    
-        const removeButton = document.createElement('button')
-        removeButton.className = ('remove-button');
-        removeButton.innerHTML = ('X');
-        newBook.appendChild(removeButton);
+            else if(library.length >= 10){
+                alert('did not bother making the page responsive, so any more than 10 entries will break the layout')
+                return
+            }
+            
+            else{
+                library.push(info)
+                    
+                const newBook = document.createElement('li');
+                const newBookTitle = document.createElement('h1');
+                const newBookAuthor = document.createElement('h2');
+                const newBookCheck = document.createElement('h2');
+                    
+                newBook.appendChild(newBookTitle);
+                newBook.appendChild(newBookAuthor);
+                newBook.appendChild(newBookCheck);
+                    
+                function checked(){
+                    if (info.check == true){
+                        return 'Complete'
+                    }
+                    else return 'Incomplete'
+                }
 
-        removeButton.addEventListener('click', removeBook);
+                newBookTitle.innerHTML += `Title: ${info.title}`
+                newBookAuthor.innerHTML += `Author: ${info.author}`
+                newBookCheck.innerHTML += `Status: ${checked()}`
+            
+                listOutput.appendChild(newBook);
+                newBook.className = ('book-entry');
+            
+                for(const value of Object.keys(library)){
+                    newBook.id = (value);
+                }
+            
+                const removeButton = document.createElement('button')
+                removeButton.className = ('remove-button');
+                removeButton.innerHTML = ('REMOVE');
+                newBook.appendChild(removeButton);
+            
+                removeButton.addEventListener('click', removeBook);
+            
+                function removeBook(){
+                    newBook.remove();
+                    library.splice(newBook.id, 1)
+                }
+                inputForm.style.visibility = ('hidden')
+                console.log(library)
 
-        function removeBook(){
-            newBook.remove();
-            library.splice(newBook.id, 1)
-        }
-        inputForm.style.visibility = ('hidden')
-
+    }       
 
     }
     function formDisplay(){
